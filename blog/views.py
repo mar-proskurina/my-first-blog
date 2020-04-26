bd2dd84395481a20c5c1247808d0d203ca91c69c
mar-proskurina <mar.proskurina@gmail.com>
2020-04-26 21:53:05 +0300

    changed the name of the blog, order of post details; disabled immediate posting, added page with drafts

--- blog/views.py
+++ blog/views.py
@@ -6,8 +6,13 @@ from django.shortcuts import redirect
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('created_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
 
 def post_detail(request, pk):
@@ -22,7 +27,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)        
     else:
@@ -37,7 +41,6 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
